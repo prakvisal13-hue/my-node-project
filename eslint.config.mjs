c2bbaf2887db7ffc575c2e44f80ac8d42052f722
prakvisal13-hue <prakvisal13@gmail.com>
2026-03-18 16:17:33 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  { ignores: ["Ignor build folders/**", "coverage/**"] },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["tests/**/*.test.js"], plugins: { jest }, rules: { ...jest.configs.recommended.rules }, languageOptions: { globals: globals.jest } },
